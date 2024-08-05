8a720b364f8621c3f1976a80360215056130fa27
Derek <sego37@gmail.com>
2024-08-05 17:04:49 -0600

    pages prep

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,37 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {createBrowserRouter, RouterProvider} from'react-router-dom';
+import ManagementPage from './pages/ManagementPage';
+import EmployeePage from './pages/EmployeePage';
+import Login from './pages/Login';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Login />
+      },
+      {
+        path: '/management',
+        element: <ManagementPage />
+      },
+      {
+        path: '/employee',
+        element: <EmployeePage />
+      }
+    ]
+  }
+]);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
 
