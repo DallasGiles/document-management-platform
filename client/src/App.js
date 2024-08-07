f48903afedc169f2e75caa90b173b69df486dcac
DallasGiles <105821196+DallasGiles@users.noreply.github.com>
2024-08-06 20:55:08 -0600

    added the changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,10 @@ import React from "react";
 import { ApolloProvider } from "@apollo/client";
 import client from "./apollo/client";
 import { Outlet } from "react-router-dom";
-// src/App.js
-import React, { useContext } from 'react';
-import { ApolloProvider } from '@apollo/client';
-import client from './apollo/client';
-import Home from './pages/home';
-import ForemanDashboard from './components/Dashboard/ForemanDashboard';
-import BasicUserDashboard from './components/Dashboard/BasicUserDashboard';
-import { AuthContext } from './context/AuthContext'; // Assuming you have an AuthContext to provide user info
-
+//import { AuthContext } from './context/AuthContext'; // Assuming you have an AuthContext to provide user info
 
 function App() {
-  const { user } = useContext(AuthContext);
+ //const { user } = useContext(AuthContext);
 
   return (
     <ApolloProvider client={client}>
@@ -110,13 +102,6 @@ function App() {
         <div class="z-50 relative">
           <Outlet />
         </div>
-
-        {user.role === 'Foreman' ? (
-          <ForemanDashboard />
-        ) : (
-          <BasicUserDashboard />
-        )}
-
       </div>
     </ApolloProvider>
   );
