e5b3490a3d1a5d0043176cc2c88d165e2447db67
Derek <sego37@gmail.com>
2024-08-08 20:41:21 -0600

    making pretty

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { ApolloProvider } from "@apollo/client";
 import client from "./apollo/client";
@@ -6,99 +5,27 @@ import { Outlet } from "react-router-dom";
 //import { AuthContext } from './context/AuthContext'; // Assuming you have an AuthContext to provide user info
 
 function App() {
- //const { user } = useContext(AuthContext);
+  //const { user } = useContext(AuthContext);
 
   return (
     <ApolloProvider client={client}>
       <div className="App">
-
-        <nav class="bg-white border-b border-gray-300">
-          <div class="flex justify-between items-center px-6">
-            <button id="menu-button" onclick="expandSidebar()">
-              <i class="fas fa-bars text-cyan-500 text-lg"></i>
-            </button>
+        <nav class="bg-white w-screen border-b border-gray-300">
+          <div class="flex justify-between items-center">
             <div class="mx-auto h-16 flex items-center">
-              <h1>JOB COMS</h1>
+              <h1>CONSTRUCT COMMS</h1>
             </div>
             <div class="space-x-4">
               <button>
-                <i class="fas fa-bell text-cyan-500 text-lg"></i>
+                <i class="fas fa-bell text-cyan-500 text-lg" id="notification"></i>
               </button>
               <button>
-                <i class="fas fa-user text-cyan-500 text-lg"></i>
+                <i class="fas fa-user pr-6 text-cyan-500 text-lg" id="profile"></i>
               </button>
             </div>
           </div>
         </nav>
 
-        {/* SIDEBAR */}
-        <div
-          id="sidebar"
-          class="w-auto bg-white h-screen fixed rounded-none border-none transition-all duration-200 ease-in-out overflow-hidden"
-        >
-          <div class="p-2 space-y-4">
-            <button
-              class="relative px-3 py-3 flex items-center space-x-4 justify-start text-gray-500 rounded-lg group"
-              onclick="highlightSidebarItem(this)"
-            >
-              <i class="fas fa-home text-lg"></i>
-              <span class="font-medium transition-all duration-200 opacity-0">
-                Inicio
-              </span>
-            </button>
-
-            <button
-              class="relative px-3 py-3 flex items-center space-x-4 justify-start text-gray-500 rounded-lg group"
-              onclick="highlightSidebarItem(this)"
-            >
-              <i class="fas fa-check-circle"></i>
-              <span class="font-medium transition-all duration-200 opacity-0">
-                Autorizaciones
-              </span>
-            </button>
-
-            <button
-              class="relative px-3 py-3 flex items-center space-x-4 justify-start text-gray-500 rounded-lg group"
-              onclick="highlightSidebarItem(this)"
-            >
-              <i class="fas fa-users"></i>
-              <span class="font-medium transition-all duration-200 opacity-0">
-                Usuarios
-              </span>
-            </button>
-
-            <button
-              class="relative px-3 py-3 flex items-center space-x-4 justify-start text-gray-500 rounded-lg group"
-              onclick="highlightSidebarItem(this)"
-            >
-              <i class="fas fa-store"></i>
-              <span class="font-medium transition-all duration-200 opacity-0">
-                Comercios
-              </span>
-            </button>
-
-            <button
-              class="relative px-3 py-3 flex items-center space-x-4 justify-start text-gray-500 rounded-lg group"
-              onclick="highlightSidebarItem(this)"
-            >
-              <i class="fas fa-exchange-alt"></i>
-              <span class="font-medium transition-all duration-200 opacity-0">
-                Transacciones
-              </span>
-            </button>
-
-            <button
-              class="relative px-3 py-3 flex items-center space-x-4 justify-start text-gray-500 rounded-lg group"
-              onclick="highlightSidebarItem(this)"
-            >
-              <i class="fas fa-sign-out-alt"></i>
-              <span class="font-medium transition-all duration-200 opacity-0">
-                Cerrar sesión
-              </span>
-            </button>
-          </div>
-        </div>
-        {/* SIDEBAR ENDS */}
         <div class="z-50 relative">
           <Outlet />
         </div>
@@ -107,72 +34,4 @@ function App() {
   );
 }
 
-
-{
-  /* <script>
-    function expandSidebar() {
-        const sidebar = document.getElementById('sidebar');
-        const mainContent = document.querySelector('.ml-16');
-
-        if (sidebar.style.width === '16rem') {
-            sidebar.style.width = '4rem';
-            mainContent.style.marginLeft = '4rem';
-            sidebar.classList.remove('text-left', 'px-6');
-            sidebar.classList.add('text-center', 'px-0');
-        } else {
-            sidebar.style.width = '16rem';
-            mainContent.style.marginLeft = '16rem';
-            sidebar.classList.add('text-left', 'px-6');
-            sidebar.classList.remove('text-center', 'px-0');
-        }
-
-        const labels = sidebar.querySelectorAll('span');
-        labels.forEach(label => label.classList.toggle('opacity-0'));
-    }
-
-    function highlightSidebarItem(element) {
-    const buttons = document.querySelectorAll("#sidebar button");
-    buttons.forEach(btn => {
-        btn.classList.remove('bg-gradient-to-r', 'from-cyan-400', 'to-cyan-500', 'text-white', 'w-48', 'ml-0');
-        btn.firstChild.nextSibling.classList.remove('text-white');
-    });
-    element.classList.add('bg-gradient-to-r', 'from-cyan-400', 'to-cyan-500', 'w-56', 'h-10','ml-0');
-    element.firstChild.nextSibling.classList.add('text-white');
-    }
-
-    // Para la gráfica de Usuarios
-    var ctx = document.getElementById('usersChart').getContext('2d');
-    new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-            labels: ['Usuarios Nuevos', 'Usuarios Registrados'],
-            datasets: [{
-                data: [50, 50],
-                backgroundColor: ['cyan', 'yellow'],
-            }]
-        },
-        options: {
-            responsive: true,
-        }
-    });
-
-    // Para la gráfica de Comercios
-    var ctx2 = document.getElementById('commercesChart').getContext('2d');
-    new Chart(ctx2, {
-        type: 'doughnut',
-        data: {
-            labels: ['Comercios Nuevos', 'Comercios Registrados'],
-            datasets: [{
-                data: [60, 40],
-                backgroundColor: ['cyan', 'yellow'],
-            }]
-        },
-        options: {
-            responsive: true,
-        }
-    });
-</script> */
-}
-
 export default App;
-
--- client/src/pages/ManagementPage.jsx
+++ client/src/pages/ManagementPage.jsx
@@ -1,8 +1,8 @@
 function ManagementPage() {
   return (
     <>
-      <div class="ml-16 bg-gray-100 h-screen fixed w-full lg:w-3/4 transition-all duration-200 ease-in-out">
-        <div class="flex items-center w-full mt-2 p-4">
+      <div class="bg-gray-300 w-full lg:w-3/4 transition-all duration-200 ease-in-out">
+        <div class="flex items-center w-full mt-2">
           {/* <div class="relative w-full">
             <span class="absolute inset-y-0 left-0 pl-3 flex items-center">
               <i class="fas fa-search text-gray-200"></i>
@@ -11,116 +11,98 @@ function ManagementPage() {
               type="text"
               class="pl-10 pr-4 py-2 rounded-full border border-gray-300 w-full text-sm placeholder-gray-400"
               placeholder="Buscar..."
-            />
+            /
           </div> */}
         </div>
 
-        <div class="grid grid-cols-1 gap-4 mt-2 p-4">
+        <div class="grid grid-cols-1 gap-4 p-4">
           <div class="bg-white p-4 rounded-md">
-            <h2 class="text-gray-600 text-lg font-semibold pb-4">Usuarios</h2>
+            <h2 class="text-gray-600 text-lg font-semibold pb-4">
+              Current Project
+            </h2>
             <div
-              class="chart-container"
+              class="chart-container, flex items-center justify-center"
               // style="position: relative; height:200px; width:200px"
-              style={{
-                position: "relative",
-                height: "550px",
-                width: "500px",
-              }}
             >
               {/* <canvas id="usersChart"></canvas> */}
-              <object class="pdf" 
-            data=
-"Blueprint2-1.pdf"
-            width="800"
-            height="500">
-    </object>
+              <object
+                class="pdf"
+                data="Blueprint2-1.pdf"
+                width="140%"
+                height="500"
+              ></object>
             </div>
           </div>
 
           <div class="bg-white p-4 rounded-md">
-            <h2 class="text-gray-600 text-lg font-semibold pb-4">Comercios</h2>
+            <h2 class="text-gray-600 text-lg font-semibold">
+              Project Details
+            </h2>
             <div
               class="chart-container"
               style={{
                 position: "relative",
                 height: "200px",
-                width: "200px",
-              }}
-            >
+                width: "auto",
+              }}             
+            > <div class="flex w-auto h-full items-center justify-center shadow-lg mt-3 p-5 mb-4">
+            <p > PLACE HOLDER </p>       
+            </div>      
               <canvas id="commercesChart"></canvas>
             </div>
           </div>
 
           <div class="bg-white p-4 rounded-md">
-            <h2 class="text-gray-600 text-lg font-semibold pb-4">
-              Autorizaciones Pendientes
+            <h2 class="text-gray-600 text-lg font-semibold">
+              Create New Details To project
             </h2>
-            <table class="w-full table-auto">
-              <thead>
-                <tr class="text-sm leading-normal">
-                  <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">
-                    Foto
-                  </th>
-                  <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">
-                    Nombre
-                  </th>
-                  <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light text-right">
-                    Rol
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr class="hover:bg-grey-lighter">
-                  <td class="py-4 px-6 border-b border-grey-light">
-                    <img
-                      src="https://via.placeholder.com/40"
-                      alt="Foto Perfil"
-                      class="rounded-full h-10 w-10"
-                    />
-                  </td>
-                  <td class="py-4 px-6 border-b border-grey-light">
-                    Juan Pérez
-                  </td>
-                  <td class="py-4 px-6 border-b border-grey-light text-right">
-                    Administrador
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-
-          <div class="bg-white p-4 rounded-md">
-            <h2 class="text-gray-600 text-lg font-semibold pb-4">
-              Transacciones
-            </h2>
-            <table class="w-full table-auto">
-              <thead>
-                <tr class="text-sm leading-normal">
-                  <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">
-                    Nombre
-                  </th>
-                  <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">
-                    Fecha
-                  </th>
-                  <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light text-right">
-                    Monto
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr class="hover:bg-grey-lighter">
-                  <td class="py-4 px-6 border-b border-grey-light">
-                    Carlos Sánchez
-                  </td>
-                  <td class="py-4 px-6 border-b border-grey-light">
-                    27/07/2023
-                  </td>
-                  <td class="py-4 px-6 border-b border-grey-light text-right">
-                    $1500
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <div class="flex mx-auto items-center justify-center shadow-lg mt-5 mb-4">
+              <form class="flex w-auto bg-white rounded-lg px-4 pt-2">
+                <div class="flex flex-wrap -mx-3 mb-6">
+                  <div class="w-full md:w-full px-3 mb-2 mt-2">
+                    <textarea
+                      class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"
+                      name="body"
+                      placeholder="Type Your Details"
+                      required
+                    ></textarea>
+                  </div>
+                  <div class="w-full md:w-full flex items-start md:w-full px-3">
+                    <div class="flex items-start w-1/2 text-gray-700 px-2 mr-auto">
+                      <svg
+                        fill="none"
+                        class="w-5 h-5 text-gray-600 mr-1"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                      <p class="text-xs md:text-sm pt-px">
+                        when submitted, details will be public
+                      </p>
+                    </div>
+                    <div class="-mr-1">
+                      <input
+                        id="pdf-upload"
+                        type="submit"
+                        class="bg-white  text-gray-700 font-medium py-1 px-4 border border-gray-400 rounded-lg tracking-wide m-4 hover:bg-gray-100"
+                        value="+PDF"
+                      />
+                      <input
+                        type="submit"
+                        class="bg-white text-gray-700 font-medium py-1 px-4 border border-gray-400 rounded-lg tracking-wide mr-1 hover:bg-gray-100"
+                        value="Post Details"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
@@ -129,3 +111,27 @@ function ManagementPage() {
 }
 
 export default ManagementPage;
+
+{
+  /* <div class="flex mx-auto items-center justify-center shadow-lg mt-56 mx-8 mb-4 max-w-lg">
+<form class="w-full max-w-xl bg-white rounded-lg px-4 pt-2">
+   <div class="flex flex-wrap -mx-3 mb-6">
+      <h2 class="px-4 pt-3 pb-2 text-gray-800 text-lg">Add a new comment</h2>
+      <div class="w-full md:w-full px-3 mb-2 mt-2">
+         <textarea class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white" name="body" placeholder='Type Your Comment' required></textarea>
+      </div>
+      <div class="w-full md:w-full flex items-start md:w-full px-3">
+         <div class="flex items-start w-1/2 text-gray-700 px-2 mr-auto">
+            <svg fill="none" class="w-5 h-5 text-gray-600 mr-1" viewBox="0 0 24 24" stroke="currentColor">
+               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+            </svg>
+            <p class="text-xs md:text-sm pt-px">Some HTML is okay.</p>
+         </div>
+         <div class="-mr-1">
+            <input type='submit' class="bg-white text-gray-700 font-medium py-1 px-4 border border-gray-400 rounded-lg tracking-wide mr-1 hover:bg-gray-100" value='Post Comment'/>
+         </div>
+      </div>
+      </div>
+   </form>
+   </div> */
+}
