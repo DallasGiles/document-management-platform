69fc91a3caab738b3a00b10d60cf99d425e2daef
Derek <sego37@gmail.com>
2024-08-12 18:14:15 -0600

    fine tuned pages

--- client/src/pages/ManagementPage.jsx
+++ client/src/pages/ManagementPage.jsx
@@ -3,16 +3,7 @@ function ManagementPage() {
     <>
       <div class="bg-gray-300 w-full lg:w-3/4 transition-all duration-200 ease-in-out">
         <div class="flex items-center w-full mt-2">
-          {/* <div class="relative w-full">
-            <span class="absolute inset-y-0 left-0 pl-3 flex items-center">
-              <i class="fas fa-search text-gray-200"></i>
-            </span>
-            <input
-              type="text"
-              class="pl-10 pr-4 py-2 rounded-full border border-gray-300 w-full text-sm placeholder-gray-400"
-              placeholder="Buscar..."
-            /
-          </div> */}
+        
         </div>
 
         <div class="grid grid-cols-1 gap-4 p-4">
@@ -22,9 +13,9 @@ function ManagementPage() {
             </h2>
             <div
               class="chart-container, flex items-center justify-center"
-              // style="position: relative; height:200px; width:200px"
+             
             >
-              {/* <canvas id="usersChart"></canvas> */}
+             
               <object
                 class="pdf"
                 data="Blueprint2-1.pdf"
@@ -94,6 +85,7 @@ function ManagementPage() {
                         value="+PDF"
                       />
                       <input
+                        id = "submit"
                         type="submit"
                         class="bg-white text-gray-700 font-medium py-1 px-4 border border-gray-400 rounded-lg tracking-wide mr-1 hover:bg-gray-100"
                         value="Post Details"
@@ -112,26 +104,3 @@ function ManagementPage() {
 
 export default ManagementPage;
 
-{
-  /* <div class="flex mx-auto items-center justify-center shadow-lg mt-56 mx-8 mb-4 max-w-lg">
-<form class="w-full max-w-xl bg-white rounded-lg px-4 pt-2">
-   <div class="flex flex-wrap -mx-3 mb-6">
-      <h2 class="px-4 pt-3 pb-2 text-gray-800 text-lg">Add a new comment</h2>
-      <div class="w-full md:w-full px-3 mb-2 mt-2">
-         <textarea class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white" name="body" placeholder='Type Your Comment' required></textarea>
-      </div>
-      <div class="w-full md:w-full flex items-start md:w-full px-3">
-         <div class="flex items-start w-1/2 text-gray-700 px-2 mr-auto">
-            <svg fill="none" class="w-5 h-5 text-gray-600 mr-1" viewBox="0 0 24 24" stroke="currentColor">
-               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
-            </svg>
-            <p class="text-xs md:text-sm pt-px">Some HTML is okay.</p>
-         </div>
-         <div class="-mr-1">
-            <input type='submit' class="bg-white text-gray-700 font-medium py-1 px-4 border border-gray-400 rounded-lg tracking-wide mr-1 hover:bg-gray-100" value='Post Comment'/>
-         </div>
-      </div>
-      </div>
-   </form>
-   </div> */
-}
